b7aed2b70c61bf24b2636f2328166294af9d6379
leessang10 <leessang10@naver.com>
2022-04-14 20:34:51 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -0,0 +1,12 @@
+package hello.itemservice.web.item.basic;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+}
